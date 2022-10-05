4a6b767b270a6a8fcb4bd7ce9a32e609fe3e146b
sunwenhaopro <2196728181@qq.com>
2022-10-05 15:16:04 +0800

    886

---
+++ 罗盘时钟/Untitled-1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+void ReadFile(char*s)
+{
+
+}
+void Change(char*s1,char*s2)
+{
+
+}
+int main()
+{
+    fstream io;
+    io.open("ff.txt",ios::out);
+
+
+}
